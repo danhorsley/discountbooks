1765b32443c4fec99ea5f20082aaa5811f61e24d
danhorsley <danielhorsley@me.com>
2021-09-25 14:11:57 +0100

    cleaned analysis data

--- dbviz/pop.py
+++ dbviz/pop.py
@@ -13,7 +13,7 @@ def dateconverter(my_date, type = 'inv'):
 def pop_static(reset = True):
     if reset:
         StaticData.objects.all().delete()  
-    with open("dbviz/new_static.csv", "r") as f:
+    with open("dbviz/static.csv", "r") as f:
             reader = csv.reader(f)
             for row in reader:
                 #print(row[2])
@@ -24,6 +24,27 @@ def pop_static(reset = True):
                         genre2 = row[6], length = row[7],
                         width = row[8],thick = row[9], weight = row[10])
                 sd.save()
+
+def pop_analysis(reset = True):
+    if reset:
+        AnalysisData.objects.all().delete()  
+    counter = -1
+    with open("dbviz/analysis.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+                counter +=1
+                if counter ==0:
+                    pass
+                else:
+                    print(row[0])
+                    #print(not StaticData.objects.filter(isbn13=row[0]))
+                    if not StaticData.objects.filter(isbn13=row[0]):
+                        pass
+                    else:
+                        ad = AnalysisData(book = StaticData.objects.filter(isbn13=row[0])[0], 
+                            sellpx = row[2], offers = row[3], ninetyd = row[4],
+                            stock = row[5], asr = row[6])
+                        ad.save()
 
 def pop_invoice(reset = True):
     if reset:
