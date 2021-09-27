b97a4e3c7153c59d6f2bea79bd74316fd8f70835
danhorsley <danielhorsley@me.com>
2021-09-27 10:53:31 +0100

    new accurate query and plot

--- dbviz/plots.py
+++ dbviz/plots.py
@@ -8,26 +8,42 @@ def sqldate_to_datetime(my_date):
     return datetime.strptime(my_date, '%Y-%m-%d %H:%M:%S+00:00').date() 
 
 def sales_by_book():
-    my_agg = StaticData.objects.values('title').order_by('title')\
-                                    .annotate(total_s=Sum('salesdata__price'))\
-                                    .annotate(total_q=Sum('salesdata__quantity'))\
-                                    .annotate(total_f=Sum('salesdata__salesfees'))\
-                                    .annotate(total_post=Sum('salesdata__postage'))\
-                                    .annotate(total_inv_cost=Sum(F('invoicedata__cost')*F('invoicedata__quantity')))\
-                                    .annotate(total_inv_qty=Sum(F('invoicedata__quantity')))\
-                                    .annotate(wavg_cost = (F('total_inv_cost')/F('total_inv_qty')))\
-        .annotate(total_p = (F('total_s') + F('total_f') + F('total_post') - (F('wavg_cost')*F('total_q'))))
-    return my_agg
+    sales_agg = SalesData.objects.values('book_id').order_by('book_id')\
+                                    .annotate(total_s=Sum('price'))\
+                                    .annotate(total_q=Sum('quantity'))\
+                                    .annotate(total_f=Sum('salesfees'))\
+                                    .annotate(total_post=Sum('postage'))
+
+    invoice_agg = InvoiceData.objects.values('book_id').order_by('book_id')\
+                .annotate(total_inv_cost=Sum(F('cost')*F('quantity')))\
+                .annotate(total_inv_qty=Sum(F('quantity')))\
+                .annotate(wavg_cost = (F('total_inv_cost')/F('total_inv_qty')))
+
+    invoice_dict = {y['book_id']:[y['total_inv_cost'], y['total_inv_qty'], 
+                                y['wavg_cost']] for y in invoice_agg}
+
+    static_dict = {x[0]: [x[2], x[1], x[4], x[5]] for x in StaticData.objects.values_list()}
+    
+    all_info = [[z['book_id'], 
+                static_dict[z['book_id']][0], static_dict[z['book_id']][1], 
+                static_dict[z['book_id']][2], static_dict[z['book_id']][3], 
+                z['total_s'], z['total_q'], z['total_f'], z['total_post'],
+                invoice_dict[z['book_id']][0], invoice_dict[z['book_id']][1],
+                invoice_dict[z['book_id']][2], 
+                z['total_s'] + z['total_f'] + z['total_post'] - (invoice_dict[z['book_id']][2]*z['total_q'])]\
+                for z in sales_agg]
+        
+    return all_info
 
 def plot_format(my_plot):
     return my_plot.update_layout(autosize=False, width=800, height=493,
     legend_orientation="h", margin_t=25, margin_b=25, margin_r=25, margin_l=50,
     yaxis=go.layout.YAxis(titlefont=dict(size=15), tickformat='d'))
 
-def sales_chart(how = 'best', many = 15):
+def sales_chart(how = 'best', many = 15, measure = 'profit'):
+    m_dict = {'profit' : 12, 'quantity' : 6}
     sbb = sales_by_book()
-    def no_none(s): return 0 if s is None else s
-    ordered_sales = [[x['title'][:22], no_none(x['total_p'])] for x in sbb]
+    ordered_sales = [[x[1][:22], x[m_dict[measure]]] for x in sbb]
     ordered_sales.sort(key=lambda x: x[1])
     if how == 'best': myf = ordered_sales[-many:]
     else: myf = ordered_sales[:many]
@@ -36,3 +52,6 @@ def sales_chart(how = 'best', many = 15):
     plot_format(my_plot)
     return my_plot.show() 
  
+def genre_chart():
+    sbb = sales_by_book()
+
