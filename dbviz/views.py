8596ae4ddba58f67fcb0400d3160993b0e0022da
danhorsley <danielhorsley@me.com>
2021-10-03 13:51:53 +0100

    agg data for notebook export

--- dbviz/views.py
+++ dbviz/views.py
@@ -90,19 +90,14 @@ def dataquery(request):
                                                 "default_menus" : default_menus
                                                 })
 
-def download_csv(request, queryset):
-    opts = queryset.model._meta
-    model = queryset.model
-    response = HttpResponse(mimetype='text/csv')
-    # force download.
-    response['Content-Disposition'] = 'attachment;filename=export.csv'
-    # the csv writer
-    writer = csv.writer(response)
-    field_names = [field.name for field in opts.fields]
-    # Write a first row with header information
-    writer.writerow(field_names)
-    # Write data rows
-    for obj in queryset:
-        writer.writerow([getattr(obj, field) for field in field_names])
-    return response
-download_csv.short_description = "Download selected as csv"
+def download_csv():
+
+    agg = StaticData.objects.all().select_related().annotate(tp=Sum('salesdata__profit'))\
+                                    .annotate(tq=Sum('salesdata__quantity'))
+    with open('nb.csv', 'w') as f:
+        writer = csv.writer(f)
+        field_names = [field.name for field in agg.model._meta.fields] + ['tp', 'tq']
+        writer.writerow(field_names)
+        for obj in agg:
+            writer.writerow([getattr(obj, field) for field in field_names])
+    return print('done')
